e68d4d4abeef52cecc24529daab06b44c687334d
hezheli <hezheli@rcrai.com>
2025-06-06 20:14:51 +0800

    修复未定义的符号警告

--- entry/napi_module.c
+++ entry/napi_module.c
@@ -6,14 +6,14 @@ static char modname_buf[128];
 static napi_module nativeModule = {
     .nm_version = 1,
     .nm_flags = 0,
-    .nm_filename = nullptr,
+    .nm_filename = NULL,
     .nm_register_func = InitializeModule,
     .nm_modname = modname_buf,
-    .nm_priv = nullptr,
+    .nm_priv = NULL,
     .reserved = {0},
 };
 
 void register_module(const char *modname) {
-  strcpy(nativeModule.nm_modname, modname);
+  strcpy(modname_buf, modname);
   napi_module_register(&nativeModule);
 }
